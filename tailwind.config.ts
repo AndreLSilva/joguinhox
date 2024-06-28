162d3e1a82825a33743c8db0eff392a7cd8916b3
André Lima e Silva <andrelimaesilvag@gmail.com>
2024-06-27 22:25:49 -0300

    000: Initial setup - ALS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,60 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+  plugins: [],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontSize: {
+      "title-lg": [
+        "6.75rem",
+        {
+          fontWeight: "400",
+          letterSpacing: "-0.09375rem",
+          lineHeight: "6.75rem",
+        },
+      ],
+      "title-md": [
+        "3.375rem",
+        {
+          fontWeight: "500",
+          letterSpacing: "0rem",
+          lineHeight: "3.375rem",
+        },
+      ],
+      "title-sm": [
+        "2.375rem",
+        {
+          fontWeight: "600",
+          letterSpacing: "0rem",
+          lineHeight: "2.375rem",
+        },
+      ],
+      "subtitle-large": [
+        "1.75rem",
+        {
+          fontWeight: "500",
+          letterSpacing: "0rem",
+          lineHeight: "1.75rem",
+        },
+      ],
+      subtitle: [
+        "1.375rem",
+        {
+          fontWeight: "500",
+          letterSpacing: "0.009375rem",
+          lineHeight: "1.375rem",
+        },
+      ],
+      body: [
+        "1.25rem",
+        {
+          fontWeight: "500",
+          letterSpacing: "0.009375rem",
+          lineHeight: "1.25rem",
+        },
+      ],
     },
   },
-  plugins: [],
 };
+
 export default config;
